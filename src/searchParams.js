6df07271324d26d4215b1c6ff8499ba567381763
James Lopez <james@jamess-mbp.mynetworksettings.com>
2022-06-07 11:47:10 -0400

    drop down list added

--- src/searchParams.js
+++ src/searchParams.js
@@ -1,8 +1,12 @@
 import { useState } from "react";
 
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+
 const SearchParams = () => {
   //const location = "Seattle, WA";
   const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
+
   return (
     <div className="search-params">
       <form>
@@ -14,6 +18,30 @@ const SearchParams = () => {
             placeholder="Location"
             onChange={(e) => setLocation(e.target.value)}
           ></input>
+        </label>
+        <label htmlFor="animals">
+          animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+            onBlur={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+          >
+            <option />
+            {ANIMALS.map((animal) => {
+              return (
+                <option key={animal} value={animal}>
+                  {animal}
+                </option>
+              );
+            })}
+          </select>
         </label>
         <button>Submit</button>
       </form>
