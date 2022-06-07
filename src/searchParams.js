d24b37f2f9e339f68fce5288484f300aecf33171
James Lopez <james@jamess-mbp.mynetworksettings.com>
2022-06-07 10:15:30 -0400

    useState added

--- src/searchParams.js
+++ src/searchParams.js
@@ -1,11 +1,19 @@
+import { useState } from "react";
+
 const SearchParams = () => {
-  const location = "Seattle, WA";
+  //const location = "Seattle, WA";
+  const [location, setLocation] = useState("");
   return (
     <div className="search-params">
       <form>
         <label htmlFor="location">
           Location
-          <input id="location" value={location} placeholder="Location"></input>
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={(e) => setLocation(e.target.value)}
+          ></input>
         </label>
         <button>Submit</button>
       </form>
