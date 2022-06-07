97cf7ae046d86276244ee1a08fab583a5a6862ec
James Lopez <james@jamess-mbp.mynetworksettings.com>
2022-06-07 10:10:44 -0400

    Search Component Added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese" />
-      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
-      <Pet name="Doing" animal="Cat" breed="Mix" />
+      <SearchParams />
     </div>
   );
 };
 
-ReactDOM.render(<App />, document.getElementById("root"));
+render(<App />, document.getElementById("root"));
