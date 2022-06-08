911749f9ab4525fc48b23daf5ac0d947421e284d
James Lopez <james@jamess-mbp.mynetworksettings.com>
2022-06-08 16:55:35 -0400

    added class properties

--- src/Details.js
+++ src/Details.js
@@ -1,8 +1,47 @@
 import { useParams } from "react-router-dom";
+import { Component } from "react";
 
-const Details = () => {
-  const { id } = useParams();
-  return <h2>{id}</h2>;
+class Details extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { loading: true };
+  }
+
+  async componentDidMount() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.params.id}`
+    );
+    const json = await res.json();
+
+    this.setState(Object.assign({ loading: false }, json.pets[0]));
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <h2>loading ...</h2>;
+    }
+
+    const { animal, breed, city, state, description, name } = this.state;
+
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>
+            {animal} - {breed} - {city}, {state}
+          </h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+const WrappedDetails = () => {
+  const params = useParams();
+  return <Details params={params} />;
 };
 
-export default Details;
+export default WrappedDetails;
